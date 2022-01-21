471a6557b6eca72c298c0aee6db685db40466181
Maarten Engels <maartene@mac.com>
2022-01-21 09:00:25 +0100

    Initial Commit

---
+++ MiniStealth/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  MiniStealth
+//
+//  Created by Maarten Engels on 21/01/2022.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
