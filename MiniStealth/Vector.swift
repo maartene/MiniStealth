4db721e2ea33bb481008528389a31d93d3cb6c6d
Maarten Engels <maartene@mac.com>
2022-03-14 11:14:34 +0100

    episode 04

--- MiniStealth/Vector.swift
+++ MiniStealth/Vector.swift
@@ -11,8 +11,30 @@ struct Vector {
     var x: Int
     var y: Int
     
+    // MARK: computed properties to quickly get common values
     static var zero: Vector {
         Vector(x: 0, y: 0)
+    }
+    
+    static var left: Vector {
+        Vector(x: -1, y: 0)
+    }
+    
+    static var right: Vector {
+        Vector(x: 1, y: 0)
+    }
+    
+    static var up: Vector {
+        Vector(x: 0, y: 1)
+    }
+    
+    static var down: Vector {
+        Vector(x: 0, y: -1)
+    }
+    
+    // MARK: vector arithmatic
+    static func +(lhs: Vector, rhs: Vector) -> Vector {
+        Vector(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
     }
 }
 
