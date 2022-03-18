7520c1ef27a8dcc5146a9ec4233f7317f7604bc1
Maarten Engels <maartene@mac.com>
2022-03-18 15:57:47 +0100

    episode 05

---
+++ MiniStealth/Model/WorldUpdateable.swift
@@ -0,0 +1,12 @@
+//
+//  WorldUpdateable.swift
+//  MiniStealth
+//
+//  Created by Maarten Engels on 14/03/2022.
+//
+
+import Foundation
+
+protocol WorldUpdateable {
+    func update(in world: World)
+}
