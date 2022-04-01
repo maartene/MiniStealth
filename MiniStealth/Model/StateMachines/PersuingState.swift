1f3c56331802fc6fe1fb4e22d6323a63ffa54c11
Maarten Engels <maartene@mac.com>
2022-04-01 16:15:14 +0200

    episode07

--- MiniStealth/Model/StateMachines/PersuingState.swift
+++ MiniStealth/Model/StateMachines/PersuingState.swift
@@ -24,26 +24,38 @@ final class PersuingState: GKState, WorldUpdateable, OwnedState {
     }
     
     func update(in world: World) {
-        var dX = 0
-        var dY = 0
-
-        if target.position.x > owner.position.x {
-            dX = 1
-        }
-
-        if target.position.x < owner.position.x {
-            dX = -1
-        }
-
-        if target.position.y > owner.position.y {
-            dY = 1
-        }
-
-        if target.position.y < owner.position.y {
-            dY = -1
+//        var dX = 0
+//        var dY = 0
+//
+//        if target.position.x > owner.position.x {
+//            dX = 1
+//        }
+//
+//        if target.position.x < owner.position.x {
+//            dX = -1
+//        }
+//
+//        if target.position.y > owner.position.y {
+//            dY = 1
+//        }
+//
+//        if target.position.y < owner.position.y {
+//            dY = -1
+//        }
+        
+        let path = world.map.path(from: owner.position, to: target.position)
+        
+        guard path.count > 0 else {
+            print("No valid path found from \(owner.position) to \(target.position)")
+            return
         }
         
-        let direction = Vector(x: dX, y: dY)
+        let direction: Vector
+        if path.count == 1 { // either we are already at the right place or the path only contains the target position.
+            direction = path[0] - owner.position
+        } else {
+            direction = path[1] - owner.position
+        }
         
         if let newHeading = Heading.vectorToHeading(direction) {
             owner.heading = newHeading
